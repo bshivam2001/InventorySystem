7fde13b7f3b1bce4201c65c3d294c8f8ed6c42c7
Shivam Bhardwaj <bshivam2001@gmail.com>
2022-07-09 01:53:06 +0530

    initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong>Shivam Bhardwaj - Yuvraj Gajani - Arunesh Sarmah</strong>
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
--- stock.sql
+++ stock.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2018 at 10:20 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Generation Time: Apr 18, 2022 at 05:51 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -77,7 +76,10 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`id`, `name`, `active`) VALUES
-(4, 'ABC Inc.', 1);
+(4, 'ABC Inc.', 1),
+(5, 'Godrej', 1),
+(6, 'Amul', 1),
+(7, 'Bajaj', 1);
 
 -- --------------------------------------------------------
 
@@ -96,7 +98,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `active`) VALUES
-(4, 'Microscope', 1);
+(4, 'Home and Kitchen', 1),
+(5, 'Food and Grocery', 1);
 
 -- --------------------------------------------------------
 
@@ -141,7 +144,9 @@ CREATE TABLE `groups` (
 
 INSERT INTO `groups` (`id`, `group_name`, `permission`) VALUES
 (1, 'Administrator', 'a:36:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createBrand\";i:9;s:11:\"updateBrand\";i:10;s:9:\"viewBrand\";i:11;s:11:\"deleteBrand\";i:12;s:14:\"createCategory\";i:13;s:14:\"updateCategory\";i:14;s:12:\"viewCategory\";i:15;s:14:\"deleteCategory\";i:16;s:11:\"createStore\";i:17;s:11:\"updateStore\";i:18;s:9:\"viewStore\";i:19;s:11:\"deleteStore\";i:20;s:15:\"createAttribute\";i:21;s:15:\"updateAttribute\";i:22;s:13:\"viewAttribute\";i:23;s:15:\"deleteAttribute\";i:24;s:13:\"createProduct\";i:25;s:13:\"updateProduct\";i:26;s:11:\"viewProduct\";i:27;s:13:\"deleteProduct\";i:28;s:11:\"createOrder\";i:29;s:11:\"updateOrder\";i:30;s:9:\"viewOrder\";i:31;s:11:\"deleteOrder\";i:32;s:11:\"viewReports\";i:33;s:13:\"updateCompany\";i:34;s:11:\"viewProfile\";i:35;s:13:\"updateSetting\";}'),
-(4, 'Owners', 'a:36:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createBrand\";i:9;s:11:\"updateBrand\";i:10;s:9:\"viewBrand\";i:11;s:11:\"deleteBrand\";i:12;s:14:\"createCategory\";i:13;s:14:\"updateCategory\";i:14;s:12:\"viewCategory\";i:15;s:14:\"deleteCategory\";i:16;s:11:\"createStore\";i:17;s:11:\"updateStore\";i:18;s:9:\"viewStore\";i:19;s:11:\"deleteStore\";i:20;s:15:\"createAttribute\";i:21;s:15:\"updateAttribute\";i:22;s:13:\"viewAttribute\";i:23;s:15:\"deleteAttribute\";i:24;s:13:\"createProduct\";i:25;s:13:\"updateProduct\";i:26;s:11:\"viewProduct\";i:27;s:13:\"deleteProduct\";i:28;s:11:\"createOrder\";i:29;s:11:\"updateOrder\";i:30;s:9:\"viewOrder\";i:31;s:11:\"deleteOrder\";i:32;s:11:\"viewReports\";i:33;s:13:\"updateCompany\";i:34;s:11:\"viewProfile\";i:35;s:13:\"updateSetting\";}');
+(4, 'Owners', 'a:36:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createBrand\";i:9;s:11:\"updateBrand\";i:10;s:9:\"viewBrand\";i:11;s:11:\"deleteBrand\";i:12;s:14:\"createCategory\";i:13;s:14:\"updateCategory\";i:14;s:12:\"viewCategory\";i:15;s:14:\"deleteCategory\";i:16;s:11:\"createStore\";i:17;s:11:\"updateStore\";i:18;s:9:\"viewStore\";i:19;s:11:\"deleteStore\";i:20;s:15:\"createAttribute\";i:21;s:15:\"updateAttribute\";i:22;s:13:\"viewAttribute\";i:23;s:15:\"deleteAttribute\";i:24;s:13:\"createProduct\";i:25;s:13:\"updateProduct\";i:26;s:11:\"viewProduct\";i:27;s:13:\"deleteProduct\";i:28;s:11:\"createOrder\";i:29;s:11:\"updateOrder\";i:30;s:9:\"viewOrder\";i:31;s:11:\"deleteOrder\";i:32;s:11:\"viewReports\";i:33;s:13:\"updateCompany\";i:34;s:11:\"viewProfile\";i:35;s:13:\"updateSetting\";}'),
+(5, 'BillDesk', 'a:14:{i:0;s:8:\"viewUser\";i:1;s:9:\"viewGroup\";i:2;s:11:\"updateBrand\";i:3;s:9:\"viewBrand\";i:4;s:14:\"updateCategory\";i:5;s:12:\"viewCategory\";i:6;s:9:\"viewStore\";i:7;s:15:\"updateAttribute\";i:8;s:13:\"viewAttribute\";i:9;s:13:\"updateProduct\";i:10;s:11:\"viewProduct\";i:11;s:11:\"updateOrder\";i:12;s:9:\"viewOrder\";i:13;s:11:\"viewReports\";}'),
+(6, 'Store Manager', 'a:33:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"updateGroup\";i:5;s:9:\"viewGroup\";i:6;s:11:\"deleteGroup\";i:7;s:11:\"createBrand\";i:8;s:11:\"updateBrand\";i:9;s:9:\"viewBrand\";i:10;s:11:\"deleteBrand\";i:11;s:14:\"createCategory\";i:12;s:14:\"updateCategory\";i:13;s:12:\"viewCategory\";i:14;s:14:\"deleteCategory\";i:15;s:11:\"updateStore\";i:16;s:9:\"viewStore\";i:17;s:11:\"deleteStore\";i:18;s:15:\"createAttribute\";i:19;s:15:\"updateAttribute\";i:20;s:13:\"viewAttribute\";i:21;s:15:\"deleteAttribute\";i:22;s:13:\"createProduct\";i:23;s:13:\"updateProduct\";i:24;s:11:\"viewProduct\";i:25;s:13:\"deleteProduct\";i:26;s:11:\"createOrder\";i:27;s:11:\"updateOrder\";i:28;s:9:\"viewOrder\";i:29;s:11:\"deleteOrder\";i:30;s:11:\"viewReports\";i:31;s:11:\"viewProfile\";i:32;s:13:\"updateSetting\";}');
 
 -- --------------------------------------------------------
 
@@ -167,6 +172,13 @@ CREATE TABLE `orders` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `bill_no`, `customer_name`, `customer_address`, `customer_phone`, `date_time`, `gross_amount`, `service_charge_rate`, `service_charge`, `vat_charge_rate`, `vat_charge`, `net_amount`, `discount`, `paid_status`, `user_id`) VALUES
+(2, 'BILPR-EE2E', 'XYZ', 'Street1', '1233456678', '1650296916', '60.00', '13', '7.80', '10', '6.00', '73.80', '', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -182,6 +194,13 @@ CREATE TABLE `orders_item` (
   `amount` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders_item`
+--
+
+INSERT INTO `orders_item` (`id`, `order_id`, `product_id`, `qty`, `rate`, `amount`) VALUES
+(4, 2, 4, '1', '60', '60.00');
+
 -- --------------------------------------------------------
 
 --
@@ -196,12 +215,21 @@ CREATE TABLE `products` (
   `qty` varchar(255) NOT NULL,
   `image` text NOT NULL,
   `description` text NOT NULL,
-  `attribute_value_id` text,
+  `attribute_value_id` text DEFAULT NULL,
   `brand_id` text NOT NULL,
   `category_id` text NOT NULL,
   `store_id` int(11) NOT NULL,
   `availability` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `sku`, `price`, `qty`, `image`, `description`, `attribute_value_id`, `brand_id`, `category_id`, `store_id`, `availability`) VALUES
+(2, 'Toaster', '11111', '1999', '500', '<p>You did not select a file to upload.</p>', '<p>Electronic Toast Maker </p>', 'null', '[\"7\"]', '[\"4\"]', 3, 1),
+(3, 'Stove', '11112', '4499', '50', '<p>You did not select a file to upload.</p>', '<p>4 Channel Stove with Auto Start Feature</p>', 'null', '[\"5\"]', '[\"4\"]', 3, 1),
+(4, 'Milk', '22221', '60', '499', '<p>You did not select a file to upload.</p>', '<p>Amul Homogenised Milk</p>', 'null', '[\"6\"]', '[\"5\"]', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -214,6 +242,13 @@ CREATE TABLE `stores` (
   `name` varchar(255) NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`, `active`) VALUES
+(3, 'Store', 1);
 
 -- --------------------------------------------------------
 
@@ -354,13 +389,13 @@ ALTER TABLE `attribute_value`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `company`
@@ -372,31 +407,31 @@ ALTER TABLE `company`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders_item`
 --
 ALTER TABLE `orders_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
